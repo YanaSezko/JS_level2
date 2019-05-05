82e3d6ca33ee7af0f72e0f7d2fc74f7a9a4c7873
Yana <33156136+YanaSezko@users.noreply.github.com>
2019-05-05 20:52:07 +0300

    Lesson1

---
+++ lesson1/script.js
@@ -0,0 +1,31 @@
+//с заданием 2 пока не разобралась,но в процессе.
+const goods = [
+	{ title:"Shirt", price:"200" },
+	{ title:"Socks", price:"100" },
+	{ title:"Jacket", price:"300" },
+	{ title:"Shoes", price:"400" },
+	{ title:"Shirt", price:"200" },
+	{ title:"Socks", price:"100" },
+	{ title:"Jacket", price:"300" },
+	{ title:"Shoes", price:"400" },
+	{ title:"Shirt", price:"200" },
+	{ title:"Socks", price:"100" },
+	{ title:"Jacket", price:"300" },
+	{ title:"Shoes", price:"400" }
+]
+
+const renderGoodsItem = (title, price) => {
+ return `<div class="goods-item">
+<h3>${title}</h3>
+<p>${price}</p>
+<img src="http://dummyimage.com/120" />
+</div>`;
+}
+
+const renderGoodsList = (list) => {
+const goodsList = list.map(item =>
+renderGoodsItem(item.title, item.price));
+document.querySelector('.goods-list').innerHTML=goodsList.join('');
+}
+
+renderGoodsList(goods)
