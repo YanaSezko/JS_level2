04b53ce605f494f6536bd3f9961a13ed483b922d
Yana <33156136+YanaSezko@users.noreply.github.com>
2019-05-12 12:30:12 +0300

    Lesson2

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,30 +1,70 @@
-const goods = [
+class GoodsItem {
+	constructor(title="Без названия", price="цена не указана") {
+		this.title = title;
+		this.price = price;
+	}
+	render() {
+		return `<div class="goods-item">
+<h3>${this.title}</h3>
+<p>${this.price}</p>
+<img src="http://dummyimage.com/120" />
+<button class="btn">Добавить в корзину</button>
+</div>`;
+	}
+}
+
+class GoodsList {
+	constructor() {
+		this.goods = []
+	}
+	fetchGoods() {
+		this.goods = [
 	{ title:"Shirt", price:"200" },
 	{ title:"Socks", price:"100" },
 	{ title:"Jacket", price:"300" },
-	{ title:"Shoes", price:"400" },
+	{ title:"Shoes" },
 	{ title:"Shirt", price:"200" },
 	{ title:"Socks", price:"100" },
-	{ title:"Jacket", price:"300" },
+	{ price:"300" },
 	{ title:"Shoes", price:"400" },
 	{ title:"Shirt", price:"200" },
 	{ title:"Socks", price:"100" },
 	{ title:"Jacket", price:"300" },
 	{ title:"Shoes", price:"400" }
-]
-
-const renderGoodsItem = (title, price) => {
- return `<div class="goods-item">
-<h3>${title}</h3>
-<p>${price}</p>
-<img src="http://dummyimage.com/120" />
-</div>`;
+		];
+	}
+	
+	render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
 }
 
-const renderGoodsList = (list) => {
-const goodsList = list.map(item =>
-renderGoodsItem(item.title, item.price));
-document.querySelector('.goods-list').innerHTML=goodsList.join('');
+
+
+/*  //Класс элемента корзины товаров
+class BasketItem {
+    constructor(id, title, price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
 }
 
-renderGoodsList(goods)
+// Класс корзины товаров
+class Basket {
+    constructor() {
+        this.cartGoods = [];
+    }
+	
+    // Добавление товара в корзину 
+  
+    // Удаление товара из корзины */
+   
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
